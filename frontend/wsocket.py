988fd1a2271503b9459feb82390aefd8a2908657
Herilala ANDRIAMIADANTSOA <herilala.it@OPTISERV.local>
2026-05-29 16:18:09 +0200

    Features: implement frontend system.

--- frontend/wsocket.py
+++ frontend/wsocket.py
@@ -2,28 +2,53 @@ import websockets
 import asyncio
 import qrcode
 import json
+import threading
+import cmd
 
 
-async def connect_to_server():
-    uri = 'ws://localhost:8088'
+class WTerminal (cmd.Cmd):
+    intro = """ Welcome to WTerminal """
+    prompt = "wterminal >_ :"
 
-    # Se connecter au websocket génerer par le backend
-    async with websockets.connect(uri) as ws:
-        print(f"[+] Connected to the backend")
+    def __init__(self):
+        super().__init__()
+        self.ws = None
+        self.loop = asyncio.new_event_loop()
+        self.net_threading = threading.Thread(
+            target=self.start_net_loop, daemon=True
+        )
+        self.net_threading.start()
 
-        while True:
-            message = await ws.recv()
-            data = json.loads(message)
+    def start_net_loop(self):
+        asyncio.set_event_loop(self.loop)
+        self.loop.run_until_complete(self.connect_to_backend)
 
-            if (data['type'] == 'QR'):
-                print(f"[+] Please scan QR code\n\n")
-                
-                # Afficher le code QR en utilisant le module qrcode
-                qr = qrcode.QRCode()
-                qr.add_data(data['data'])
-                qr.make()
-                qr.print_ascii()
-            elif (data['type'] == 'STATUS'):
-                print(f"[+] Status : {data['data']}")
+    async def connect_to_backend(self):
+        uri = 'ws://localhost:8088'
 
-asyncio.run(connect_to_server())
+        # Se connecter au websocket génerer par le backend
+        async with websockets.connect(uri) as ws:
+            print("[+] Connected to the backend")
+
+            while True:
+                try:
+                    message = await ws.recv()
+                    data = json.loads(message)
+
+                    if (data['type'] == 'QR'):
+                        print("[+] Please scan QR code\n\n")
+                        
+                        # Afficher le code QR en utilisant le module qrcode
+                        qr = qrcode.QRCode()
+                        qr.add_data(data['data'])
+                        qr.make()
+                        qr.print_ascii()
+                    elif (data['type'] == 'STATUS'):
+                        print(f"[+] Status : {data['data']}")
+                except Exception as e:
+                    self.ws = None
+                    asyncio.sleep(2)
+                    print(f"[!] Error occured : {e}")
+
+
+asyncio.run(connect_to_backend())

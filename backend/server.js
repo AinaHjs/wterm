6c6ae1a736f2658ebe6ac971592a7f1960f6d123
Herilala ANDRIAMIADANTSOA <herilala.it@OPTISERV.local>
2026-05-29 13:52:38 +0200

    Features: Connect python app to the node backend.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import makeWASocket, {useMultiFileAuthState, DisconnectReason} from "@whiskeysockets/baileys";
 import { WebSocketServer } from "ws";
 import pino from 'pino';
+import QRCode from 'qrcode-terminal';
 
 
 /**
@@ -8,45 +9,91 @@ import pino from 'pino';
  * - 
  */
 
-/** INITIALISATION DU WEBSOCKET POUR PYTHON ET NODE */
-const wss = new WebSocketServer({port:8088});
-console.log(`[+] The node server is listenning on port : 8088`)
+/** INITIALISATION DES VARIABLES GLOBALES */
 let pythonClient = null;
+let sock = null;
 
-/** Manage inbound connexion */
-wss.on('connection', async (ws) => {
-    console.log(`[+] Python client connected successfully...`);
-    pythonClient = ws
 
-    // On message
-    ws.on('message', async (message) => {
-        try {
-            const parsed_message = JSON.parse(message); 
-            
-            if (parsed_message.action === 'SEND_MESSAGE' && parsed_message.to && parsed_message.text) {
-                await sock.sendMessage(parsed_message.to, {text:parsed_message.text})
-                console.log(`[+] Message sent to : ${parsed_message.to}`);
-            }
-        } catch (error) {
-            console.error(`[!] An error occured : ${error}`);
-        }
-    });
-
-    // On close
-    ws.on('close', () => {
-        console.log(`[-] Python client disconnected successfully...`);
-        pythonClient = null;
-    });
-
-});
 
 /** INITIALISATION DU WEBSOCKET WA AVEC BAILEYS */
-const {state, saveCreds} = await useMultiFileAuthState('auth_info_baileys');
+async function connectToWA() {
+    const {state, saveCreds} = await useMultiFileAuthState('auth_info_baileys');
 
-const sock = makeWASocket({
-    auth: state,
-    printQRInTerminal: true,
-    logger: pino({level : 'silent'}),
-})
+    sock = makeWASocket({
+        auth: state,
+        printQRInTerminal: false,
+        logger: pino({level : 'silent'}),
+    })
+
+    /** Gérer les connnections */
+    sock.ev.on('connection.update', (update) => {
+        const {connection, lastDisconnect, qr} = update;
+
+        if(qr && pythonClient) {
+            pythonClient.send(JSON.stringify({
+                type: "QR",
+                data: qr
+            }))
+        }
+
+        // Vérifier l'état de la connexion
+        if (connection === 'open' && pythonClient) {
+            console.log(`[+] Link to WA established...`);
+            console.log(`[+] Trying to send the connexion status to the python app`)
+            pythonClient.send(JSON.stringify({
+                type: 'STATUS',
+                data: 'CONNECTED'
+            }))
+        } else if (connection === 'close') {
+            const need_to_reconnect = lastDisconnect?.error?.output?.statusCode !== DisconnectReason.loggedOut;
+
+            if(need_to_reconnect) {
+                console.log(`[-] Connection to WA was lost...`);
+                console.log(`[+] Trying to reconnect...`)
+                connectToWA();
+            }
+        }
+    })
+
+    /** Sauvegarde automatique des creds */
+    sock.ev.on('creds.update',saveCreds);
+}
+
+/** INITIALISATION DU WEBSOCKET POUR PYTHON ET NODE */
+function create_ws() {
+    const wss = new WebSocketServer({port:8088});
+    console.log(`[+] The node server is listenning on port : 8088`)
+
+    /** Manage inbound connexion */
+    wss.on('connection', async (ws) => {
+        console.log(`[+] Python client connected successfully...`);
+        pythonClient = ws
+        connectToWA();
+
+        // On message
+        ws.on('message', async (message) => {
+            try {
+                const parsed_message = JSON.parse(message); 
+                
+                if (parsed_message.action === 'SEND_MESSAGE' && parsed_message.to && parsed_message.text) {
+                    await sock.sendMessage(parsed_message.to, {text:parsed_message.text})
+                    console.log(`[+] Message sent to : ${parsed_message.to}`);
+                }
+            } catch (error) {
+                console.error(`[!] An error occured : ${error}`);
+            }
+        });
+
+        // On close
+        ws.on('close', () => {
+            console.log(`[-] Python client disconnected successfully...`);
+            pythonClient = null;
+        });
+
+    });
+}
+
+
+create_ws();
 
 
--- frontend/wsocket.py
+++ frontend/wsocket.py
@@ -1,12 +1,29 @@
 import websockets
 import asyncio
+import qrcode
+import json
 
 
 async def connect_to_server():
     uri = 'ws://localhost:8088'
+
+    # Se connecter au websocket génerer par le backend
     async with websockets.connect(uri) as ws:
-        print('test')
-        await asyncio.Future()
+        print(f"[+] Connected to the backend")
+
+        while True:
+            message = await ws.recv()
+            data = json.loads(message)
+
+            if (data['type'] == 'QR'):
+                print(f"[+] Please scan QR code\n\n")
+                
+                # Afficher le code QR en utilisant le module qrcode
+                qr = qrcode.QRCode()
+                qr.add_data(data['data'])
+                qr.make()
+                qr.print_ascii()
+            elif (data['type'] == 'STATUS'):
+                print(f"[+] Status : {data['data']}")
 
 asyncio.run(connect_to_server())
-

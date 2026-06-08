48c701c01feb4e8b6cecc1c849e94d53b8742539
Herilala ANDRIAMIADANTSOA <herilala.it@OPTISERV.local>
2026-06-08 10:34:21 +0200

    Features: frontend and backend implementation done - 08/06/2026

--- README.md
+++ README.md
@@ -1,27 +1,33 @@
-# # WhatsApp Terminal Edition
+# # Author
+This repository is created by AinaHjs.
 
----
-
-# Un client WhatsApp léger en ligne de commande pour serveur Linux
-
----
-
-Il utilise une architecture Client/Serveur séparant la gestion du protocole WhatsApp (via Baileys) et l'interface utilisateur textuelle (TUI).
+# # About  
+Wterminal is a WhatsApp Terminal Edition.
 
 # # Architecture
 
 **\* Backend : ** Node.js avec `@whiskeysockets/baileys` qui tourne en arrière-plan.
 **\* Frontend : ** Interface TUI (Terminal User Interface) connectée via WebSockets.
 
+# # Programming languages
+1. Python for the frontend
+2. NodeJS for the backend
+
+# # Module installation
+1. For the backend
+- npm install @whiskeysockets/baileys ws pino
+
+2. For the frontend
+- pip install websockets qrcode colorama pillow
+
 # # Installation & Lancement
 
-1. Cloner le dépôt.
-2. Configurer le backend : `cd backend && npm install`.
-3. Lancer le serveur et scanner le QR Code : `node index.js`.
-4. Lancer l'interface dans un autre terminal.
+1. Clone the repository.
+2. Configure backend : `cd backend && npm install`.
+3. Launch the server et scan the QR Code : `node server.js`.
+4. Run wterminal on a new terminal `python app.py`.
 
-Help et les fonctionnalités : (Pour le devéloppement seulment)
-# Msg
+# # List of usefull command
 - Chats
 - Read user@
 - Send user@ msg
--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,17 @@ import makeWASocket, {useMultiFileAuthState, DisconnectReason} from "@whiskeysoc
 import { WebSocketServer } from "ws";
 import pino from 'pino';
 import QRCode from 'qrcode-terminal';
-import fs from 'fs';
+import fs from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from "url";
-
-
 
 
 /** INITIALISATION DES VARIABLES GLOBALES */
 let pythonClient = null;
 let sock = null;
+
 const __filename = fileURLToPath(import.meta.url)
-const ROOT_DIR = path.resolve(__filename);
+const ROOT_DIR = path.dirname(__filename);
 
 /** INITIALISATION DU WEBSOCKET WA AVEC BAILEYS */
 async function connectToWA() {
@@ -36,126 +35,120 @@ async function connectToWA() {
             }))
         }
 
-        // Vérifier l'état de la connexion
+        // Check connection state
         if (connection === 'open' && pythonClient) {
-            console.log(`[+] Link to WA established...`);
-            console.log(`[+] Trying to send the connexion status to the python app`)
+            console.log(`[+] Connection to whatsapp established...`);
+            console.log(`[+] Trying to send the connexion status to wterminal.`)
             pythonClient.send(JSON.stringify({
                 type: 'STATUS',
                 data: 'CONNECTED'
             }))
+    
+        // Check logout reason and try to reconnect if there was an error.
         } else if (connection === 'close') {
             const need_to_reconnect = lastDisconnect?.error?.output?.statusCode !== DisconnectReason.loggedOut;
 
             if(need_to_reconnect) {
-                console.log(`[+] Trying to reconnect...`)
+                console.log(`[+] Trying to reconnect whatsapp.`)
                 connectToWA();
             }
         }
     })
 
-    /** Sauvegarde automatique des creds */
+    // Automatically save creds
     sock.ev.on('creds.update',saveCreds);
 
+    // Listen on messages.upsert from whatsapp.
     sock.ev.on('messages.upsert', async (m) => {
-            if (m.type === 'notify') {
-                for (const msg of m.messages) {
-                    if (!msg.key.fromMe) {
-                        let senderContactName = null;
-                        const jID = msg.key.remoteJid;
-                        const name = msg.pushName;
-                        const text = msg.message?.conversation || msg.message?.extendedTextMessage;
-                        const msgDate = msg.messageTimestamp
-                        const contactsFilePath = path.join(
-                            ROOT_DIR,
-                            'Frontend',
-                            'contacts.json'
-                        );
-                        const contacts = JSON.parse(
-                            fs.readFileSync(
-                                contactsFilePath,
-                                "utf8"
-                            )
-                        );
-                        for(const key in Object.keys(contacts)){
-                            if (key === jID){
-                                senderContactName = key;
-                            };
+        if (m.type === 'notify') {
+            for (const msg of m.messages) {
+                if (!msg.key.fromMe) {
+                    const jID = msg.key.remoteJid;
+                    const name = msg.pushName ||"Unknown";
+                    const text = msg.message?.conversation || msg.message?.extendedTextMessage;
+                    const msgDate = msg.messageTimestamp;
+
+                    if (!text) continue;
+
+                    // Check if the sender is already on my contact list.
+                    let senderContactName = "Unknown"
+                    try {
+                        const contactsFilePath = path.join(ROOT_DIR,'Frontend','contacts.json');
+                        const contactData = await fs.readFile(contactsFilePath, "utf-8");
+                        const contacts = JSON.parse(contactData);
+
+                        senderContactName = Object.keys(contacts).find(key => contacts[key] === jID || jID.split("@")[0]);
+                    } catch (error) {
+                        senderContactName = jID.split("@")[0];
+                    }
+
+                    // Send to Python client if connected
+                    if(pythonClient) {
+                        const messagePayload = {
+                            event: 'NEW_MESSAGE',
+                            sender_name : name,
+                            sender_id : jID,
+                            text: text,
+                            date : msgDate,
                         };
 
-                        if (text && pythonClient) {
-                            let newMessageModel = `[${msgDate}] - ${name} \n ${text}`;
-                            const newMsgFilePath = path.join(
-                                ROOT_DIR,
-                                'Frontend',
-                                'Messages',
-                                `${senderContactName}.txt`
-                            );
-                            const newMsgListFilePath = path.join(
-                                ROOT_DIR,
-                                'Frontend',
-                                'Messages',
-                                'newMessageList.txt'
-                            );
-                            const messageObject = {
-                                event: 'NEW_MESSAGE',
-                                sender_name : name,
-                                sender_id : jID,
-                                text: text,
-                                date : msgDate,
-                            };
-                            const messagePayload = JSON.stringify(messageObject);
-                            pythonClient.send(messagePayload);
-                            // Write new message on the users' message file
-                            await fs.appendFile(
-                                newMsgFilePath,
-                                newMessageModel,
-                                "utf8"
-                            );
-                            // Write new message on the list of new message
-                            await fs.appendFile(
-                                newMsgListFilePath,
-                                `[${msgDate}] - ${name} `,
-                                "utf8"
-                            );
-                        
-                        }
+                        pythonClient.send(JSON.stringify(messagePayload));
+                    }
+
+                    // Write new message on user_message_file and list_new_message_file
+                    const newMsgFilePath = path.join(ROOT_DIR, "Frontend", "Messages", `${senderContactName.toLowerCase()}.txt`);
+                    const newMsgListFilePath = path.join(ROOT_DIR, "Frontend", "Messages", "newMessageList.txt");
+                    const newMessageModel = `${msgDate}-${senderContactName}-${text}.`
+
+                    try {
+                        await fs.appendFile(newMsgFilePath, newMessageModel, "utf8");
+                        await fs.appendFile(newMsgListFilePath, `[${msgDate}] : New message from ${senderContactName} \n>>> ${text}.`)
+                    } catch (err) {
+                        console.log(`[!] Error writing message file : ${err}`)
                     }
                 }
             }
+        }
     })
 }
 
 /** INITIALISATION DU WEBSOCKET POUR PYTHON ET NODE */
 function create_ws() {
     const wss = new WebSocketServer({port:8088});
-    console.log(`[+] The node server is listenning on port : 8088`)
+    console.log(`[+] The backend server is listenning on port : 8088`)
 
-    /** Manage inbound connexion */
+    // Manage inbound connection
     wss.on('connection', async (ws) => {
-        console.log(`[+] Python client connected successfully...`);
+        console.log(`[+] Wterminal client connected successfully...`);
         pythonClient = ws
         connectToWA();
 
-        // On message
-        ws.on('message', async (message) => {
+        // Send status if you are connected to whatsapp
+        if (sock && sock.user) {
+            ws.send(JSON.stringify({type: 'STATUS', data: 'CONNECTED'}));
+        }
 
+        // When wterminal client send message via websocket
+        ws.on('message', async (wsMessage) => {
             try {
-                const parsed_message = JSON.parse(message); 
-                
-                if (parsed_message.action === 'SEND_MESSAGE' && parsed_message.to && parsed_message.text) {
-                    await sock.sendMessage(parsed_message.to, {text:parsed_message.text})
-                    console.log(`[+] Message sent to : ${parsed_message.to}`);
+                const data = JSON.parse(wsMessage); 
+    
+                if (data.action === 'SEND_MESSAGE' && data.to && data.text) {
+                    if (sock){
+                        await sock.sendMessage(data.to, {text:data.text});
+                        console.log(`[+] Message sent to : ${data.to}`);
+                    }
+                } else {
+                    console.log("[!] Whatsapp socket not initialized yet.");
                 }
-                
-            } catch (error) {
-                console.error(`[!] An error occured : ${error}`);
+            } catch (err) {
+                console.error(`[!] Socket error : ${err}`);
             }
         });
 
-        // On close
+        // When wterminal client is disconnected
         ws.on('close', () => {
-            console.log(`[-] Python client disconnected successfully...`);
+            console.log(`[-] Wterminal client disconnected successfully...`);
             pythonClient = null;
         });
 
@@ -164,5 +157,6 @@ function create_ws() {
 
 
 create_ws();
+connectToWA();
 
 
